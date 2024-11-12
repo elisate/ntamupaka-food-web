fcc59f9d453996b9496144dbaeb2e5d23ea46f76
elisate <dushimiyimanaelisa@gmail.com>
2024-11-12 05:38:22 +0200

    good deployment

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
-// vite.config.js
-export default {
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
   build: {
-    outDir: "build", // Change output directory to 'build'
+    rollupOptions: {
+      external: [],
+    },
   },
-};
+});
